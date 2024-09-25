6240331104f7fcb5b20f80deffed157cb163a0f1
gpreciat <gapreciat@gmail.com>
2024-09-24 21:44:24 -0600

    updated pipeline and model generation

--- 2023_iDopaNeuro/driver_createMultipleIDNModels.m
+++ 2023_iDopaNeuro/driver_createMultipleIDNModels.m
@@ -2,94 +2,82 @@
 % function XomicsToMultipleModels. 
 %
 % The batch models are saved in 
-% ~/work/sbgCloud/programReconstruction/projects/exoMetDN/results/codeResults/multidimensionalModelGeneration
+% workingDir/multidimensionalModelGeneration
 
 
 %% Set directory and specificData
 
-clear
-% specificData folder
-dataFolder = ['~' filesep 'work' filesep 'sbgCloud' filesep 'programReconstruction' ...
-    filesep 'projects' filesep 'exoMetDN' filesep 'data' filesep 'xomics'];
+clearvars -except workingDir CBTDIR
 
-%filenames
+dataFolder = [CBTDIR filesep 'tutorials' filesep 'dataIntegration' filesep ...
+    'XomicsToModel' filesep 'iDopaNeuro' filesep 'data'];
+
+% Omics data filenames
 bibliomicData = 'bibliomicData.xlsx';
 exometabolomicData = 'exometabolomicData.txt';
 transcriptomicData = 'transcriptomicData.txt';
 
-% generic model
-inputFolder = ['~' filesep 'work' filesep 'sbgCloud' filesep 'programExperimental' ...
-    filesep 'projects' filesep 'xomics' filesep 'data' filesep 'Recon3D_301'];
-genericModelName = 'Recon3DModel_301_xomics_input.mat';
+% % % % generic model
+% % % inputFolder = ['~' filesep 'work' filesep 'sbgCloud' filesep 'programExperimental' ...
+% % %     filesep 'projects' filesep 'xomics' filesep 'data' filesep 'Recon3D_301'];
+% % % genericModelName = 'Recon3DModel_301_xomics_input.mat';
 
-%% automatic processing from here on
-%bibliomic data
+
+%% Automatic processing from here on
+
+% Bibliomic data
 specificData = preprocessingOmicsModel([dataFolder filesep bibliomicData], 1, 1);
 
-%exometabolomic data
+% Exometabolomic data
 specificData.exoMet = readtable([dataFolder filesep exometabolomicData]);
 
-%transcriptomic data
+% Transcriptomic data
 specificData.transcriptomicData = readtable([dataFolder filesep transcriptomicData]);
 specificData.transcriptomicData.genes = string(specificData.transcriptomicData.genes);
 
-replaceModels = false;
-
-%load generic model
-load([inputFolder filesep genericModelName])
+% Load generic model
+load([CBTDIR filesep 'papers' filesep '2023_iDopaNeuro' filesep 'Recon3DModel_301_thermo.mat'])
 modelGenerationConditions.genericModel.model = model;
 
-if contains(char(java.lang.System.getProperty('user.name')),'rfleming')
-    %action = 'debug';
-    %action = 'oneModel';
-    action = 'batch';
-    action = 'iDopaNeuroC';
-else
-    action = 'batch';
-end
+
+%action = 'debug';
+action = 'batch';
+% action = 'iDopaNeuroC';
+% action = 'iDopaNeuroCT';
 
 switch action
     case 'batch'
-        modelsDir = ['~' filesep 'work' filesep 'sbgCloud' filesep 'programReconstruction'...
-            filesep 'projects' filesep 'exoMetDN' filesep 'results' filesep 'codeResults' filesep 'multidimensionalModelGeneration'];
+        modelsDir = workingDir;
         if ~isfolder(modelsDir)
             mkdir(modelsDir);
         end
-%         cd(modelsDir)
-        
-        % Define results directory
-        modelGenerationConditions.outputDir = modelsDir;
+
         % modelGenerationConditions
+        modelGenerationConditions.outputDir = modelsDir;        
         % Note: if the data does not vary it is enough to declare them in param.
         % Here the non-varying conditions are left for demonstrative purposes
-        %modelGenerationConditions.cobraSolver = {'gurobi'}; %  {'ibm_cplex', 'gurobi'};
-        %modelGenerationConditions.cobraSolver = {'ibm_cplex'}; %  {'ibm_cplex', 'gurobi'};
         modelGenerationConditions.cobraSolver = {'mosek'}; 
-        modelGenerationConditions.activeGenesApproach = {'deleteModelGenes', 'oneRxnPerActiveGene'}; % {'deleteModelGenes', 'oneRxnPerActiveGene'};
+        modelGenerationConditions.activeGenesApproach = {'deleteModelGenes', 'oneRxnPerActiveGene'}; 
         modelGenerationConditions.transcriptomicThreshold = [0 2]; % [0 1 2];
-        modelGenerationConditions.boundsToRelaxExoMet = {'b'}; % {'l', 'b', 'u'}
-        modelGenerationConditions.closeIons = [true false]; % [true false];
-        modelGenerationConditions.tissueSpecificSolver = {'fastCore', 'thermoKernel'}; % {'fastCore', 'thermoKernel'};
-        modelGenerationConditions.inactiveGenesTranscriptomics = [true false]; % [true false];
-        modelGenerationConditions.limitBounds = [1e4 1e5]; % [1e3 1e4 1e5];
-        modelGenerationConditions.curationOverOmics = [true false]; % [true false];
+        modelGenerationConditions.closeIons = [true false]; 
+        modelGenerationConditions.tissueSpecificSolver = {'fastCore', 'thermoKernel'}; 
+        modelGenerationConditions.inactiveGenesTranscriptomics = [true false]; 
+        modelGenerationConditions.limitBounds = [1e4 1e5]; 
+        modelGenerationConditions.curationOverOmics = [true false]; 
+        modelGenerationConditions.activeOverInactive = [true false];        
         param.debug = false;
         param.printLevel = 1;
         
     case 'iDopaNeuroC'
         % thermoKernel_oneRxnPerActiveGene_transcriptomicsT2_limitBoundary.10000_inactiveGenesT_closedIons_omicsOverCuration
-
-        modelsDir = ['~' filesep 'work' filesep 'sbgCloud' filesep 'programReconstruction' ...
-            filesep 'projects' filesep 'exoMetDN' filesep 'results' filesep 'iDN1'];
+        modelsDir = [workingDir filesep 'iDopaNeuroC'];
         if ~isfolder(modelsDir)
             mkdir(modelsDir);
         end
-        cd(modelsDir)
         
-        % Define results directory
-        modelGenerationConditions.outputDir = modelsDir;
-        
-        modelGenerationConditions.cobraSolver = {'ibm_cplex'};
+        % modelGenerationConditions
+        modelGenerationConditions.outputDir = modelsDir;        
+        modelGenerationConditions.cobraSolver = {'mosek'};
         modelGenerationConditions.activeGenesApproach = {'oneRxnPerActiveGene'};
         modelGenerationConditions.transcriptomicThreshold = 2;
         modelGenerationConditions.boundsToRelaxExoMet = {'both'};
@@ -103,17 +91,13 @@ switch action
         
     case 'iDopaNeuroCT'
         % thermoKernel_oneRxnPerActiveGene_transcriptomicsT2_limitBoundary.10000_inactiveGenesT_closedIons_curationOverOmics
-
-        modelsDir = ['~' filesep 'work' filesep 'sbgCloud' filesep 'programReconstruction' ...
-            filesep 'projects' filesep 'exoMetDN' filesep 'results' filesep 'iDN1'];
+        modelsDir = [workingDir filesep 'iDopaNeuroCT'];
         if ~isfolder(modelsDir)
             mkdir(modelsDir);
         end
-        cd(modelsDir)
-        
-        % Define results directory
-        modelGenerationConditions.outputDir = modelsDir;
-        
+
+        % modelGenerationConditions
+        modelGenerationConditions.outputDir = modelsDir;        
         modelGenerationConditions.cobraSolver = {'ibm_cplex'};
         modelGenerationConditions.activeGenesApproach = {'oneRxnPerActiveGene'};
         modelGenerationConditions.transcriptomicThreshold = 2;
@@ -125,22 +109,19 @@ switch action
         modelGenerationConditions.curationOverOmics = true;
         param.debug = true;
         param.printLevel = 2;
+
     case 'debug'
         
-        modelsDir = ['~' filesep 'work' filesep 'sbgCloud' filesep 'programReconstruction' ...
-            filesep 'projects' filesep 'exoMetDN' filesep 'results' filesep 'iDopaNeuro1debug'];
+        modelsDir = [workingDir filesep 'debug'];
         if ~isfolder(modelsDir)
             mkdir(modelsDir);
         end
-        cd(modelsDir)
-        
-        % Define results directory
-        modelGenerationConditions.outputDir = modelsDir;
-        
+                            
         %debug generation of a single model type
         % modelGenerationConditions
         % Note: if the data does not vary it is enough to declare them in param.
         % Here the non-varying conditions are left for demonstrative purposes
+        modelGenerationConditions.outputDir = modelsDir;
         modelGenerationConditions.cobraSolver = {'gurobi'}; %  {'ibm_cplex', 'gurobi'};
         modelGenerationConditions.activeGenesApproach = {'deleteModelGenes'}; % {'deleteModelGenes', 'oneRxnPerActiveGene'};
         modelGenerationConditions.transcriptomicThreshold = 0; % [0 1 2];
@@ -170,7 +151,6 @@ param.addCoupledRxns = 1;
 param.nonCoreSinksDemands = 'closeAll';
 param.closeUptakes = true; % Cell culture information
 param.findThermoConsistentFluxSubset = 1;
-
 
 %% Create models
 
